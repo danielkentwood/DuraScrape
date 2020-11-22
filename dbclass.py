b65bf703688b695279fc5f69aae239886356150b
daniel wood <danielkentwood@gmail.com>
2020-11-22 00:48:16 -0600

    intial commit for rebranding to DuraScrape
    functional crawler for Journal of Neurophysiology

--- dbclass.py
+++ dbclass.py
@@ -1,9 +1,8 @@
 import logging
+from configparser import ConfigParser
+from collections import defaultdict
 
 import psycopg2 as pg
-
-import yaml
-
 import pandas as pd
 
 
@@ -13,41 +12,65 @@ log = logging.getLogger(__name__)
 class Database:
     def __init__(self):
         self.conn = self.get_database()
+        
+    def config(self, filename='database.ini', section='postgresql'):
+        # create a parser
+        parser = ConfigParser()
+        # read config file
+        parser.read(filename)
+
+        # get section, default to postgresql
+        db = {}
+        if parser.has_section(section):
+            params = parser.items(section)
+            for param in params:
+                db[param[0]] = param[1]
+        else:
+            raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+            
+        return db
+
+    def pgrs_connect(self):
+        """ Connect to the PostgreSQL database server """
+        conn = None
+        try:
+            # read connection parameters
+            params = self.config()
+
+            # connect to the PostgreSQL server
+            print('Connecting to the PostgreSQL database: {}'.format(params['database']))
+            conn = pg.connect(**params)
+
+            # create a cursor
+            cur = conn.cursor()
+
+            # execute a statement
+            print('PostgreSQL database version:')
+            cur.execute('SELECT version()')
+
+            # display the PostgreSQL database server version
+            db_version = cur.fetchone()
+            print(db_version)
+        except (Exception, pg.DatabaseError) as error:
+            print(error)
+
+        return conn
+
+    def disconnect(self):
+        if not self.conn.closed:
+            self.conn.close()
+            print('Connection is now closed.')
+        else:
+            print('Connection is already closed.')
 
     def get_database(self):
         try:
-            conn = self.get_connection_from_profile()
+            conn = self.pgrs_connect()
             log.info("Connected to PostgreSQL database.")
         except IOError:
-            log.exception("Failed to get database connection.")
+            log.exception("Failed to get database connection from .ini profile.")
             return None, 'fail'
         return conn
-
-    def get_connection_from_profile(self,
-                                    config_file_name="default_profile.yaml"):
-        """
-        Sets up database connection from config file.
-        """
-        with open(config_file_name, 'r') as f:
-            vals = yaml.load(f)
-        if not ('HOST' in vals.keys() and
-                'USER' in vals.keys() and
-                'PASSWORD' in vals.keys() and
-                'DBNAME' in vals.keys() and
-                'PORT' in vals.keys()):
-            raise Exception('Bad config file: ' + config_file_name)
-
-        return self.get_connection(vals['DBNAME'], vals['USER'],
-                                   vals['HOST'], vals['PORT'],
-                                   vals['PASSWORD'])
-
-    def get_connection(self, db, user, host, port, passwd):
-        """
-        Get connection using credentials.
-        """
-        conn_string = "host={} port={} dbname={} user={} password={}".\
-            format(host, str(port), db, user, passwd)
-        return pg.connect(conn_string)
 
     def terminate_all_other_connections(self):
         sql="""
@@ -56,7 +79,7 @@ class Database:
         WHERE pid <> pg_backend_pid( ) \
         AND datname = current_database( );
         """
-        with self.db.conn.cursor() as cursor:
+        with self.conn.cursor() as cursor:
             cursor.execute(sql)
 
     def create_tables(self):
@@ -78,23 +101,189 @@ class Database:
             cursor.execute(meta_sql)
             text_sql = '''
                 CREATE TABLE IF NOT EXISTS body (
-                id bigint,
+                id SERIAL primary key,
+                meta_id bigint,
                 sectionA text,
                 sectionB text,
                 prose text
             );'''
             cursor.execute(text_sql)
         self.conn.commit()
+        
+    def list_tables(self):
+        query = """
+        SELECT table_schema, table_name
+        FROM information_schema.tables
+        WHERE (table_schema = 'public')
+        ORDER BY table_schema, table_name;
+        """
+        with self.conn.cursor() as cursor:
+            cursor.execute(query)
+            list_tables = cursor.fetchall()
 
+            print('** CURRENT TABLES **')
+            for t_name_table in list_tables:
+                print(t_name_table[1])
+
+    def pandas_metadata(self):
+        """
+        Returns the 'metadata' table as a pandas dataframe.
+        """
+        
+        sql_command = "SELECT * FROM metadata"
+        df = pd.read_sql(sql_command, self.conn)
+        return df
+
+    def pandas_body(self):
+        """
+        Returns the 'body' table as a pandas dataframe.
+        """
+        
+        sql_command = "SELECT * FROM body"
+        df = pd.read_sql(sql_command, self.conn)
+        return df    
+
+    def get_unique_id(self):
+        sql = """\
+        SELECT MAX(id) \
+        FROM metadata"""
+        with self.conn.cursor() as cursor:
+            cursor.execute(sql)
+            uid = cursor.fetchone()[0]
+            if not isinstance(uid,int):
+                return 0
+            else:
+                return uid + 1
+
+    def article_exists(self, title, year):
+        """
+        Given a title and a year, checks if the article exists in the database.
+        """
+        
+        sql = """\
+        SELECT COUNT(*) \
+        FROM metadata \
+        WHERE title = %s \
+        AND year = %s"""
+        args = (title, year)
+        with self.conn.cursor() as cursor:
+            cursor.execute(sql, args)
+            return cursor.fetchone()[0] > 0
+
+    def drop_empty_text_sections(self):
+        """
+        Find and drop sections in the body table 
+        that have no text in them.
+        """
+        sql = """
+        DELETE FROM body \
+        WHERE COALESCE(prose, '') = '';
+        """ 
+        with self.conn.cursor() as cursor:
+            cursor.execute(sql)
+        self.conn.commit()
+        
+    def get_article_list(self, journal_name):
+        sql = """\
+        SELECT year, issue, title \
+        FROM metadata \
+        WHERE journal = {}"""
+        args = "'" + journal_name + "'"
+        data = pd.read_sql(sql.format(args), self.conn)
+        return data
+
+    def get_meta_from_title_and_year(self, title, year):
+        sql = """\
+        SELECT * \
+        FROM metadata \
+        WHERE title = %s \
+        AND year = %s"""
+        args = (title, year)
+        meta_keys = ['id', 'url', 'journal', 'year',
+                     'volume', 'issue', 'title', 'authors',
+                     'doi', 'citations', 'cited_by']
+        with self.conn.cursor() as cursor:
+            cursor.execute(sql, args)
+            meta_vals = list(cursor.fetchone())
+        meta = dict(zip(meta_keys, meta_vals))
+        return meta
+
+    def insert_metadata(self, meta):
+        sql = """\
+        INSERT INTO metadata (\
+        id,url,journal,year,volume,issue,title,authors,doi,citations,cited_by\
+        ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+        args = (meta['id'], meta['url'], meta['journal'], meta['year'], meta['volume'],
+                meta['issue'], meta['title'], meta['authors'], meta['doi'],
+                meta['citations'], meta['cited_by'])
+        with self.conn.cursor() as cursor:
+            cursor.execute(sql, args)
+        self.conn.commit()
+
+    def insert_citations(self, article):
+        ref_id_list = []
+        # Loop through the references
+        for c in article.refs.keys():
+            # For each ref, see if it exists. If not, build it and insert.
+            # If it does exist, grab the metadata
+            if not self.article_exists(article.refs[c]['title'], 
+                                       article.refs[c]['year']):
+                meta = article.initialize_meta()
+                meta['id'] = self.get_unique_id()
+                meta['url'] = article.refs[c]['url']
+                meta['year'] = article.refs[c]['year']
+                meta['title'] = article.refs[c]['title']
+                meta['volume'] = article.refs[c]['volume']
+                meta['journal'] = article.refs[c]['journal']
+                meta['authors'] = article.refs[c]['authors']
+                self.insert_metadata(meta)
+            else:
+                meta = self.get_meta_from_title_and_year(article.refs[c]['title'], 
+                                                         article.refs[c]['year'])
+            # Add the current article's id to cited article's metadata
+            sql = """\
+            UPDATE metadata \
+            SET cited_by = array_cat(cited_by, ARRAY[%s]) \
+            WHERE id = %s"""
+            args = (article.meta['id'], meta['id'])
+            with self.conn.cursor() as cursor:
+                cursor.execute(sql, args)
+            ref_id_list.append(meta['id'])
+        # Now add the list of cited ids to the current article's metadata
+        sql = """\
+        UPDATE metadata \
+        SET citations = array_cat(citations, %s) \
+        WHERE id = %s"""
+        args = (ref_id_list, article.meta['id'])
+        with self.conn.cursor() as cursor:
+            cursor.execute(sql, args)
+        self.conn.commit()
+
+    def insert_text(self, article):
+        for key in article.section.keys():
+            sql = """\
+            INSERT INTO body (\
+            meta_id,sectiona,prose\
+            ) VALUES (%s, %s, %s)"""
+            args = (article.meta['id'], key, article.section[key])
+            with self.conn.cursor() as cursor:
+                cursor.execute(sql, args)
+        self.conn.commit()
+    
+    def insert_article(self, article):
+        self.insert_metadata(article.meta)
+        self.insert_citations(article)
+        self.insert_text(article)
+        print('Article at {} inserted into DB.'.format(article.url))
+        
 
 class Article():
     def __init__(self):
         self.meta = self.initialize_meta()
-        self.section = {}
+        self.section = defaultdict()
         self.refs = {}
         self.url = ''
         self.page = ''
-        self.db = Database()
 
     def initialize_meta(self):
         meta = {}
@@ -111,151 +300,5 @@ class Article():
         meta['cited_by'] = []
         return meta
 
-    def get_unique_id(self):
-        sql = """\
-        SELECT MAX(id) \
-        FROM metadata"""
-        with self.db.conn.cursor() as cursor:
-            cursor.execute(sql)
-            uid = cursor.fetchone()[0]
-            if not isinstance(uid,int):
-                return 0
-            else:
-                return uid + 1
 
-    def article_exists(self, title, year):
-        sql_command = """\
-        SELECT COUNT(*) \
-        FROM metadata \
-        WHERE title = %s \
-        AND year = %s"""
-        args = (title, year)
-        with self.db.conn.cursor() as cursor:
-            cursor.execute(sql_command, args)
-            return cursor.fetchone()[0] > 0
-
-    def get_article_list(self, journal_name):
-        sql_command = """\
-        SELECT year, issue, title \
-        FROM metadata \
-        WHERE journal = {}"""
-        args = "'" + journal_name + "'"
-        data = pd.read_sql(sql_command.format(args), db.conn)
-        return data
-
-    def get_meta_from_title_and_year(self, title, year):
-        sql_command = """\
-        SELECT * \
-        FROM metadata \
-        WHERE title = %s \
-        AND year = %s"""
-        args = (title, year)
-        meta_keys = ['id', 'url', 'journal', 'year',
-                     'volume', 'issue', 'title', 'authors',
-                     'doi', 'citations', 'cited_by']
-        with self.db.conn.cursor() as cursor:
-            cursor.execute(sql_command, args)
-            meta_vals = list(cursor.fetchone())
-        meta = dict(zip(meta_keys, meta_vals))
-        return meta
-
-    def insert_metadata(self, meta):
-        sql_command = """\
-        INSERT INTO metadata (\
-        id,url,journal,year,volume,issue,title,authors,doi,citations,cited_by\
-        ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
-        args = (meta['id'], meta['url'], meta['journal'], meta['year'], meta['volume'],
-                meta['issue'], meta['title'], meta['authors'], meta['doi'],
-                meta['citations'], meta['cited_by'])
-        with self.db.conn.cursor() as cursor:
-            cursor.execute(sql_command, args)
-        self.db.conn.commit()
-
-    def insert_citations(self):
-        ref_id_list = []
-        # Loop through the references
-        for c in self.refs:
-            # For each ref, see if it exists. If not, build it and insert.
-            # If it does exist, grab the metadata
-            if not self.article_exists(self.refs[c]['title'], 
-                                       self.refs[c]['year']):
-                meta = self.initialize_meta()
-                meta['id'] = self.get_unique_id()
-                meta['url'] = self.refs[c]['url']
-                meta['year'] = self.refs[c]['year']
-                meta['title'] = self.refs[c]['title']
-                meta['journal'] = self.refs[c]['journal']
-                meta['authors'] = self.refs[c]['authors']
-                self.insert_metadata(meta)
-            else:
-                meta = self.get_meta_from_title_and_year(self.refs[c]['title'], 
-                                                         self.refs[c]['year'])
-            # Add the current article's id to cited article's metadata
-            sql = """\
-            UPDATE metadata \
-            SET cited_by = array_cat(cited_by, ARRAY[%s]) \
-            WHERE id = %s"""
-            args = (self.meta['id'], meta['id'])
-            with self.db.conn.cursor() as cursor:
-                cursor.execute(sql, args)
-            ref_id_list.append(meta['id'])
-        # Now add the list of cited ids to the current article's metadata
-        sql = """\
-        UPDATE metadata \
-        SET citations = array_cat(citations, %s) \
-        WHERE id = %s"""
-        print(self.meta['id'], meta['id'])
-        args = (ref_id_list, self.meta['id'])
-        with self.db.conn.cursor() as cursor:
-            cursor.execute(sql, args)
-        self.db.conn.commit()
-
-    def insert_text(self):
-        for key in self.section.keys():
-            sql_command = """\
-            INSERT INTO body (\
-            id,sectiona,prose\
-            ) VALUES (%s, %s, %s)"""
-            args = (self.meta['id'], key, self.section[key])
-            with self.db.conn.cursor() as cursor:
-                cursor.execute(sql_command, args)
-        self.db.conn.commit()
-
-    def pandas_metadata(self):
-        sql_command = "SELECT * FROM metadata"
-        data = pd.read_sql(sql_command, self.db.conn)
-        return data
-
-    def pandas_body(self):
-        sql_command = "SELECT * FROM body"
-        data = pd.read_sql(sql_command, self.db.conn)
-        return data
-
-    def drop_empty_text_sections(self):
-        """
-        Find and drop sections in the body table 
-        that have no text in them.
-        """
-        sql = """
-        DELETE FROM body \
-        WHERE COALESCE(prose, '') = '';
-        """ 
-        with self.db.conn.cursor() as cursor:
-            cursor.execute(sql)
-        self.db.conn.commit()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
+ 
--- journal_scrape.py
+++ journal_scrape.py
@@ -1,42 +1,46 @@
 import re
+import time
+from random import random
+from collections import defaultdict
 
 from bs4 import BeautifulSoup as Bs
-
-from dbclass import Article
-
 from requests import get
+import pandas as pd
+
+from dbclass import Database, Article
 
 
 class JNeurophys:
     """
-    This is a docstring for the class that parses the Journal of Neuro
-    psychology.
+    This is a class that parses the Journal of Neurophysiology.
     """
 
-    def __init__(self):
+    def __init__(self, db=Database()):
         self.journal = 'J Neurophysiol'
         self.start_URL = 'https://journals.physiology.org/loi/jn/group/d1940.y1940'
         self.base_URL = 'https://journals.physiology.org/toc/jn/'
         self.crawl_delay = 1
+        self.db = db
 
     def get_volume_list(self):
         """
         Creates a list of the volumes in the journal. This list is the size of
         the number of issues. In other words, since there are six issues
         in each volume, each volume number repeats itself six times.
-	 """
+        """
         vol_list = []
         year_page = Bs(get(self.start_URL).text, 'html.parser')
-        all_vol = year_page.find_all('a', {'href': re.compile(r'/toc/jn/')})
+        all_vol = year_page.find_all('a', {'href': re.compile(r'/toc/jn/'), 'class': "issue__vol-issue"})
         for v in all_vol:
             try:
                 vol_str = re.search(r'Volume\s\d{1,3}', str(v)).group()
                 vol_int = int(vol_str[7:])
                 vol_list.append(vol_int)
-            except AttributeError:
-                print('Attribute Error.')
-        volume_list = vol_list.sort()
-        return volume_list
+            except:
+                print('Error: Can''t retrieve list of volumes.')
+                breakpoint()
+        vol_list.sort()
+        return vol_list
 
     def get_issue_urls(self, volume_list):
         """
@@ -49,7 +53,7 @@ class JNeurophys:
                 issue = 1
             else:
                 issue += 1
-            issue_url = self.base_url + str(v) + '/' + str(issue)
+            issue_url = self.base_URL + str(v) + '/' + str(issue)
             issue_list.append(issue_url)
         return issue_list
 
@@ -76,8 +80,9 @@ class JNeurophys:
         loa = art_page.find('div',
                             {'class': 'accordion-tabbed loa-accordion'}).\
             find_all('div', {'class': 'accordion-tabbed__tab-mobile'})
+        
         # add all the metadata fields
-        self.id = article.get_unique_id()
+        self.id = self.db.get_unique_id()
         meta = article.meta
         meta['id'] = self.id
         meta['url'] = [article.url]
@@ -103,24 +108,20 @@ class JNeurophys:
             it all in a dictionary.
         '''
         art_page = article.page
-        section = dict()
+        section=defaultdict()
         section['ABSTRACT'] = art_page.\
             select('div.hlFld-Abstract div.abstractSection')[0].get_text()
-        section['INTRODUCTION'] = ''
-        fulltext = art_page.\
-            find('div', {'class': 'hlFld-Fulltext'}).findChildren(recursive=False)
-        intro_flag = 1
+        
+        fulltext = art_page.find('div', {'class': 'hlFld-Fulltext'}).findChildren()
+        key = 'INTRODUCTION'
         for f in fulltext:
-            heading = f.find('h1', {'class': 'article-section__title section__title'})
-            if not heading:
-                section['INTRODUCTION'] = section['INTRODUCTION'] + ' ' + f.get_text()
-            else:
-                intro_flag = 0
-                section[heading.get_text()] = ''
-                for text in heading.find_parent().findChildren('div'):
-                    section[heading.get_text()] = section[heading.get_text()] + text.get_text()
-        if not intro_flag:
-            del section['INTRODUCTION']
+            title = f.name
+            if title=='h1':
+                key = f.get_text()
+                section[key] = ""
+            if title=='p':
+                section[key]+=f.get_text() + '\n\n'
+
         return section
 
     def get_references(self, article):
@@ -134,44 +135,99 @@ class JNeurophys:
         refs = dict()
         for k, r in enumerate(rlist):
             refs[k] = dict()
+            
             urls = [i.get('href') for i in r.find_all('a')]
+            year = r.find('span', {'class': 'references__year'})
+            title = r.find('span', {'class': 'references__article-title'})
+            journal = r.find('span', {'class': 'references__source'})
+            volume = r.find('i')
+            authors = r.find('span', {'class': 'references__authors'})
+            
             refs[k]['url'] = [i for i in urls if 'http' in i]
-            refs[k]['year'] = int(r.find('span', {'class': 'references__year'}).get_text()[:4])
-            refs[k]['title'] = r.\
-                find('span', {'class': 'references__article-title'}).get_text()
-            refs[k]['journal'] = r.find('span', {'class': 'references__source'}).get_text()
-            refs[k]['authors'] = r.find('span', {'class': 'references__authors'}).\
-                get_text().split(', ')
+            refs[k]['year'] = int(year.get_text()[:4]) if year else -1
+            refs[k]['title'] = title.get_text() if title else ""
+            refs[k]['volume'] = -1
+            if volume:
+                if volume.get_text().isnumeric():
+                    refs[k]['volume'] = int(volume.get_text())  
+            refs[k]['journal'] = journal.get_text() if journal else ""
+            refs[k]['authors'] = authors.get_text().split(', ') if authors else []
         return refs
 
-    def get_article(self, url):
-        ''' get_article takes a URL for an article, scrapes the metadata,
-            the text, and the references of that article, and then returns
-            dictionaries for the metadata, text, and references.
+    def get_article(self, url, art_page=""):
+        ''' 
+        get_article 
+        Takes a URL for an article, scrapes the metadata,
+        the text, and the references of that article, and then returns
+        dictionaries for the metadata, text, and references.
         '''
         article = Article()
         article.url = url
-        art_page = Bs(get(url).text, 'html.parser')
-        article.page = art_page
-        article.meta = self.get_metadata(article)
-        article.section = self.get_text(article)
-        article.refs = self.get_references(article)
-        return article
+        try: 
+            art_page = Bs(get(url).text, 'html.parser') if not art_page else art_page
+            article.page = art_page
+            article.meta = self.get_metadata(article)
+            article.section = self.get_text(article)
+            article.refs = self.get_references(article)
+            return article
+        except:
+            print('ERROR: ARTICLE NOT SAVED TO DB: {}'.format(article.url))
+            return False
+        
 
     def crawl_journal(self):
         """
         Starts from the beginning of the journal (subject to open-source
         constraints for now) and scrapes all of the articles.
         """
+        # get all volumes
         vol_list = self.get_volume_list()
+        print('journal volumes fetched')
+        # delay the crawl 
+        time.sleep(float(random() * self.crawl_delay + 1))
+        
+        # only look at open access issues for now
         open_access = [i for i in vol_list if i>76]
-        iss_list = self.get_issue_urls(open_access)
+        iss_list = list(set(self.get_issue_urls(open_access)))
+        print('journal issues fetched')
+        time.sleep(float(random() * self.crawl_delay + 1))
+        
+        # loop through issues
         for iss in iss_list:
+            print('\n********\nCurrently scraping issue at {}\n*********'.format(iss))
+            # get toc for this issue
             toc = self.get_issue_toc(iss)
-            for c in toc:
-            # check if article is behind a paywall. If so, skip it (for now)
-            if not c.find('div',{'class':'badges'}).get_text():
-                continue 
+            
+            # check the db to see if any of these articles have already been added
+            sql = """
+            SELECT DISTINCT(b.meta_id), m.url
+            FROM body AS b
+            LEFT JOIN metadata AS m on m.id = b.meta_id
+            """
+            data = pd.read_sql(sql, self.db.conn)
+            db_urls = data['url'].apply(lambda row: row[0]).to_list()
+            toc = self.get_issue_toc(iss)
+            
+            # only add articles that aren't already in the db
+            diff_toc = [e for e in toc if e not in db_urls]
+            for c in diff_toc:
+                # delay the crawl 
+                time.sleep(float(random() * self.crawl_delay + 1))
+                # check if article is behind a paywall. If so, skip it (for now)
+                art_page = Bs(get(c).text, 'html.parser')
+                if art_page.find('div',{'class':'citation__access__icon icon-Icon_Permissions-Locked'}):
+                    continue
+                else:
+                    article = self.get_article(c, art_page)
+                    if article:
+                        # (redundant) if article isn't already in the db, put it there
+                        art_exists = self.db.article_exists(
+                            article.meta['title'],
+                            article.meta['year']
+                        )
+                        if not art_exists:
+                            self.db.insert_article(article)
+                        
 
     def set_proxy(self, proxy):
         """
